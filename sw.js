2329ef8eb6335efa7768c83b29df5e53cf33c108
jaquepot77-dev <jaquepot77@gmail.com>
2026-06-12 21:02:03 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>e.waitUntil(clients.claim()));
+self.addEventListener("install",e=>self.skipWaiting());self.addEventListener("activate",e=>self.clients.claim());
